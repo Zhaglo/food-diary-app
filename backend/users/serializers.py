9631b60213dd5cc39bd61a654cb7f1b87d7bed5e
Igor Zhaglo <jagloik@gmail.com>
2026-05-14 20:25:17 +0300

    add user registration api endpoint

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 
-from .models import DailyGoals
+from .models import User, DailyGoals
 
 
 class DailyGoalsSerializer(serializers.ModelSerializer):
@@ -14,3 +15,45 @@ class DailyGoalsSerializer(serializers.ModelSerializer):
             'carbs',
         )
         read_only_fields = ('id',)
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        min_length=8,
+        validators=[validate_password],
+    )
+    password_confirm = serializers.CharField(
+        write_only=True,
+        min_length=8,
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'id',
+            'username',
+            'email',
+            'password',
+            'password_confirm',
+        )
+        read_only_fields = ('id',)
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password_confirm']:
+            raise serializers.ValidationError(
+                {'password_confirm': 'Пароли не совпадают.'}
+            )
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password_confirm')
+        password = validated_data.pop('password')
+
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+
+        DailyGoals.objects.create(user=user)
+
+        return user
