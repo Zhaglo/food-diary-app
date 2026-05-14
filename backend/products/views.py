da198446a0b6da953a0ebaba7a34e6ee6cd28ca1
Igor Zhaglo <jagloik@gmail.com>
2026-05-14 19:10:13 +0300

    add product CRUD api viewset and routing

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    serializer_class = ProductSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        return Product.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
