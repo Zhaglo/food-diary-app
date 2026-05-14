e05f55c018be9bf4b1af3050730732f181165445
Igor Zhaglo <jagloik@gmail.com>
2026-05-14 11:38:48 +0300

    create django-apps

--- backend/food_diary/settings.py
+++ backend/food_diary/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig',
+    'products.apps.ProductsConfig',
+    'diary.apps.DiaryConfig',
+    'analytics.apps.AnalyticsConfig',
 ]
 
 MIDDLEWARE = [
